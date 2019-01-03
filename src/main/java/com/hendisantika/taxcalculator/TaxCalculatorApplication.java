b9eff20a555ca21f624e6fc93ad9ef04de44097e
Hendi Santika <hendisantika@gmail.com>
2019-01-04 05:28:36 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/taxcalculator/TaxCalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.taxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxCalculatorApplication.class, args);
+    }
+
+}
+
