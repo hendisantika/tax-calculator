c2db3469992a6f418d33e34a50a3a0d4fca6cb8f
Hendi Santika <hendisantika@gmail.com>
2019-01-04 05:33:05 +0700

    #4 - Add Tax Domain

---
+++ src/main/java/com/hendisantika/taxcalculator/domain/Tax.java
@@ -0,0 +1,35 @@
+package com.hendisantika.taxcalculator.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : tax-calculator
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-04
+ * Time: 05:32
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+public class Tax {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long taxId;
+
+    private String name;
+
+    private Integer taxCode;
+
+    private Double price;
+
+    private Date created = new Date();
+}
